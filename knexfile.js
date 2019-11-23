048336cdb437bd443db8097e88bbd7f4ee283f68
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-22 20:11:40 -0600

    added knexfile and dbConfig to project

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/projects.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
+    },
+    migrations: {
+      directory: "./data/migration"
+    },
+    seeds: {
+      directory: "./data/seed"
+    }
+  }
+};
